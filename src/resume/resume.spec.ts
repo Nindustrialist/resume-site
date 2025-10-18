39beb6e2b864289e6fccaf119867792e9f43aced
Nindustrialist <nindustrialist@gmail.com>
2025-10-18 18:22:44 -0400

    fix tests and change some margins

--- src/resume/resume.spec.ts
+++ src/resume/resume.spec.ts
@@ -1,25 +1,27 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { Resume } from './resume';
 
-describe('App', () => {
+describe('Resume', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [Resume],
       providers: [provideZonelessChangeDetection()],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(Resume);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
+  it('should render my name and occupation in the title', () => {
+    const fixture = TestBed.createComponent(Resume);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, resume-site');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Collin Gibson - Software Developer',
+    );
   });
 });
